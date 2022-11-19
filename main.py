c37a3fe2147dfb6250504c2d976f215cba0ae10d
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-11-19 00:30:43 -0700

    nothing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from uwg import _1_EP_module as EP_module
 
 
 if __name__ == '__main__':
-    prompt = 'Please enter the case name: [bypass.ini]'
-    ini_file_name = input(prompt) or 'bypass.ini'
+    prompt = 'Please enter the case name: [only_uwg.ini]'
+    ini_file_name = input(prompt) or 'only_uwg.ini'
     # 1. Read configuration, Inputs/#/*.uwg, *.idf for case study
     parent.init_all(ini_file_name)
     if parent.config['Default']['software'] =='UWG_EP':
